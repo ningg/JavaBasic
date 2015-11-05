1a1b50adf115900435c19f150ffa8c2323519cf8
Guo Ning <guoning02@meituan.com>
2015-11-05 20:45:34 +0800

    add test Integer

---
+++ src/main/java/top/ningg/java/basic/TestInteger.java
@@ -0,0 +1,46 @@
+package top.ningg.java.basic;
+
+/**
+ * Created by guoning on 15/10/29.
+ */
+public class TestInteger {
+
+    public static void main(String[] args) {
+        Integer integerA = new Integer(1);
+        Integer integerB = new Integer(1);
+
+        if (integerA == integerB){
+            System.out.println("new Integer(1) == new Integer(1)");
+        }
+
+        Integer integerA1 = 1;
+        Integer integerB1 = 1;
+
+        if (integerA1 == integerB1){
+            System.out.println("整型自动装箱，两个对象相等");
+        }
+
+        Integer integerA1OfValueOf = Integer.valueOf(1);
+        Integer integerB1OfValueOf = Integer.valueOf(1);
+
+        if (integerA1OfValueOf == integerB1OfValueOf){
+            System.out.println("Integer valueOf，两个对象相等");
+        }
+
+        Integer integerA1000 = 1000;
+        Integer integerB1000 = 1000;
+
+        if (integerA1000 == integerB1000){
+            System.out.println("整型自动装箱，两个对象相等");
+        }
+
+        Integer integerA1O00fValueOf = Integer.valueOf(1000);
+        Integer integerB1O00fValueOf = Integer.valueOf(1000);
+
+        if (integerA1O00fValueOf == integerB1O00fValueOf){
+            System.out.println("Integer valueOf，两个对象相等");
+        }
+
+
+    }
+}
